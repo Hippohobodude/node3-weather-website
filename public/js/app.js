080aa6fa8dadbab705ed26498377a23635d82a3a
Jose Gabriel Amantoy <jgamantoy@live.com>
2020-12-15 00:58:36 +0800

    initial commit

---
+++ public/js/app.js
@@ -0,0 +1,27 @@
+console.log('Client side javascript file is loaded')
+
+const weatherForm = document.querySelector('form')
+const searchValue = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const location = searchValue.value
+  messageOne.textContent = 'Loading'
+  fetch(`http://localhost:3000/weather?address=${location}`).then((response)=> {
+  response.json().then((data)=> {
+    if(data.error) {
+      console.log(data.error)
+      messageTwo.textContent = data.error
+    } else {
+      messageOne.textContent = data.location
+      messageTwo.textContent = data.forecast
+      console.log(data.location) 
+      console.log(data.forecast)
+    }
+  })
+})
+
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,15 @@
+const request = require('request')
+const forecast = (lat, long, callback) => {
+  const url = 'http://api.weatherstack.com/current?access_key=faeba9b4adac1f59b149b8d904856764&query=' + lat + ',' + long + '&units=f'
+  request({url, json:true}, (error, {body})=> {
+    if (error){
+      callback('Unable to fetch weather data')
+    } else if (body.error) {
+      callback('There was a location error')
+    } else {
+      callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out`)
+    }
+  } )
+}
+
+module.exports = forecast
